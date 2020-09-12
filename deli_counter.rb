5715df62fcbfa285122dc9197745c89d05443520
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-12 19:09:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,19 +2,4 @@ katz_deli = []
 
 def line (array)
   if array.length >= 1
-    puts "The line is currently empty."
-  elsif line > 0 
-    puts |katz_deli| 
-  end
-end
-
-def take_a_number(katz_deli,name)
-  if line <= 0 
-    puts []  "#{name}"
-  elsif line > 0 
-    puts [] <<
-  end  
-end
-
-def now_serving 
-end
+    
