e64c1185f2b3a578b30745cbcec4e0290ad54870
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-12 20:17:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,7 @@
 katz_deli = []
 
 def line(array)
-  current_line = "The line is currently empty."
-  array.each.with_index(1) do |value, indexemus|
-    current_line << "#{indexemus}. #{value},"
-  end
-  puts current_line
+  if array.length >= 1 
+  
 end
     
