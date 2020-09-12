eef14eb9dbb55ff0c666686975219cba4053529e
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-12 20:13:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,8 @@ katz_deli = []
 def line(array)
   current_line = "This line is currently: "
   array.each.with_index(1) do |value, indexemus|
-    current_line << "#{indexemus}. #{value}"
+    current_line << "#{indexemus}. #{value},"
+  end
+  
+end
     
