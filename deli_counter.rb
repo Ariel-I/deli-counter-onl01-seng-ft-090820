1e5e9ee895adcc263b18661aa1663722beb242b5
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-12 18:55:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 katz_deli = []
 
-def line=(katz_deli)
+def line (katz_deli)
   if line <= 0
     puts "The line is currently empty."
   elsif line > 0 
@@ -8,7 +8,7 @@ def line=(katz_deli)
   end
 end
 
-def take_a_number= (katz_deli,name)
+def take_a_number(katz_deli,name)
   if line <= 0 
     puts [] << "
 end
