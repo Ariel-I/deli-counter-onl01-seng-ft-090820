876b9e49128bae0a27daa2fff60962bf6327fa14
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-12 20:26:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
-katz_deli = ["John", "Jacob", "Jinglehimer","Smith"]
+katz_deli = []
 
 def line(array)
   if array.length >= 1 
     newarray = []
     counter = 1 
     array.each do |name|
-      newarray.push ("#{counter}. #{name},")
+      newarray.push ("#{counter}. #{name}")
       counter += 1 
     end
     puts "The line is currently: #{newarray.join (" ")}"
